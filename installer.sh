787cec227a2e770d579d4ad91dc7931de6b3f375
mrrbrilliant <mrrbrilliantwork@gmail.com>
2021-02-15 12:05:40 +0700

    fixed empty array list and confirmations

--- installer.sh
+++ installer.sh
@@ -10,7 +10,8 @@ PURPEL='\033[0;35m'
 CYAN='\033[0;36m'
 YELLOW='\033[1;33m'
 
-retry=0
+smart_install_retries=0
+smart_update_retries=0
 continues=1
 completed=0
 
@@ -35,51 +36,156 @@ function spinner() {
     echo -e ""
 }
 
-function safe_install() {
+function smart_update() {
     # prevent stale becuase of db lock
     [[ -f "/var/lib/pacman/db.lck" ]] && sudo rm -rf /var/lib/pacman/db.lck
+    [[ $smart_update_retries < 5 ]] && echo -e "${GREEN}Smart update pass: $smart_update_retries${NC}" || echo -e "${YELLOW}Smart install pass: $smart_update_retries${NC}"
 
-    yes | sudo pacman -Syu $@ >/dev/null 2>&1 >/tmp/installation.log
+    sudo pacman -Syyu --noconfirm >/dev/null 2>&1 >/tmp/update.log
     if [[ $? -eq 1 ]]; then
         sudo find /var/cache/pacman/pkg/ -iname "*.part" -delete >/dev/null 2>&1
 
-        conflict_files=$(cat /tmp/installation.log | grep "exists in filesystem" | grep -o '/[^ ]*')
-        if [[ ${#conflict_files[@]} -gt 0 ]]; then
+        conflict_files=($(cat /tmp/update.log | grep "exists in filesystem" | grep -o '/[^ ]*'))
+
+        if [[ ${#conflict_files[@]} > 0 ]]; then
+            echo -e "Conflict files detected. Resolving conflict files"
             for ((i = 0; i < ${#conflict_files[@]}; i++)); do
-                sudo rm -rf ${conflict_files[$i]} >/dev/null 2>&1
+                sudo rm -rf ${conflict_files[$i]}
+                if [[ $? -eq 0 ]]; then
+                    echo -e "${GREEN}Removed: ${conflict_files[$i]} ${NC}"
+                else
+                    echo -e "${RED}Failed to remove: ${conflict_files[$i]} ${NC}"
+                fi
             done
         fi
 
-        conflict_packages=$(cat /tmp/installation.log | grep "are in conflict. Remove" | grep -o 'Remove [^ ]*' | grep -oE '[^ ]+$' | sed -e "s/[?]//")
-        if [[ ${#conflict_packages[@]} -gt 0 ]]; then
+        conflict_packages=($(cat /tmp/update.log | grep "are in conflict. Remove" | grep -o 'Remove [^ ]*' | grep -oE '[^ ]+$' | sed -e "s/[?]//"))
+
+        if [[ ${#conflict_packages[@]} > 0 ]]; then
+            echo -e "Conflict packages detected. Resovling conflict packages."
             for ((i = 0; i < ${#conflict_packages[@]}; i++)); do
-                yes | sudo pacman -Rcc ${conflict_packages[$i]} >/dev/null 2>&1
+                sudo pacman -Rcc --noconfirm ${conflict_packages[$i]} >/dev/null 2>&1
+                if [[ $? -eq 0 ]]; then
+                    echo -e "${GREEN}Uninstalled: ${conflict_packages[$i]} ${NC}"
+                else
+                    echo -e "${RED}Failed to uninstall: ${conflict_packages[$i]} ${NC}"
+                fi
             done
         fi
 
-        breakers=$(cat /tmp/installation.log | grep " breaks dependency " | grep -o 'required by [^ ]*' | grep -oE '[^ ]+$')
-        if [[ ${#breakers[@]} -gt 0 ]]; then
+        breakers=($(cat /tmp/update.log | grep " breaks dependency " | grep -o 'required by [^ ]*' | grep -oE '[^ ]+$'))
+
+        if [[ ${#breakers[@]} > 0 ]]; then
+            echo -e "Conflict dependencies detected. Resovling conflicting dependencies."
             for ((i = 0; i < ${#breakers[@]}; i++)); do
-                yes | sudo pacman -Rdd ${breakers[$i]} >/dev/null 2>&1
+                sudo pacman -Rdd --noconfirm ${breakers[$i]} >/dev/null 2>&1
+                if [[ $? -eq 0 ]]; then
+                    echo -e "${GREEN}Uninstalled: ${breakers[$i]} ${NC}"
+                else
+                    echo -e "${RED}Failed to uninstall: ${breakers[$i]} ${NC}"
+                fi
             done
         fi
 
-        satisfiers=$(cat /tmp/installation.log | grep "unable to satisfy dependency" | grep -oE '[^ ]+$')
+        satisfiers=($(cat /tmp/update.log | grep "unable to satisfy dependency" | grep -oE '[^ ]+$'))
+
         if [[ ${#satisfiers[@]} -gt 0 ]]; then
+            echo -e "Unsatisfied depencies detected. Resovling issues."
             for ((i = 0; i < ${#satisfiers[@]}; i++)); do
-                yes | sudo pacman -Rdd --noconfirm ${satisfiers[$i]} >/dev/null 2>&1
+                sudo pacman -Rdd --noconfirm ${satisfiers[$i]} >/dev/null 2>&1
+                if [[ $? -eq 0 ]]; then
+                    echo -e "${GREEN}Uninstalled: ${satisfiers[$i]} ${NC}"
+                else
+                    echo -e "${RED}Failed to uninstall: ${satisfiers[$i]} ${NC}"
+                fi
             done
         fi
 
-        cp /tmp/installation.log "/tmp/installation${retry}.log"
-        retry=$((retry + 1))
+        cp /tmp/update.log "/tmp/update${smart_update_retries}.log"
+        smart_update_retries=$((smart_update_retries + 1))
 
-        if [[ $retry -gt 20 ]]; then
-            safe_install $@
+        if [[ $smart_update_retries -gt 30 ]]; then
+            smart_update
         else
             continues=0
         fi
+    fi
 
+}
+
+function smart_install() {
+    # prevent stale becuase of db lock
+    [[ -f "/var/lib/pacman/db.lck" ]] && sudo rm -rf /var/lib/pacman/db.lck
+    [[ $smart_install_retries < 5 ]] && echo -e "${GREEN}Smart install pass: $smart_install_retries${NC}" || echo -e "${YELLOW}Smart install pass: $smart_install_retries${NC}"
+
+    sudo pacman -S --noconfirm $@ >/dev/null 2>&1 >/tmp/installation.log
+    if [[ $? -eq 1 ]]; then
+        sudo find /var/cache/pacman/pkg/ -iname "*.part" -delete >/dev/null 2>&1
+
+        conflict_files=($(cat /tmp/installation.log | grep "exists in filesystem" | grep -o '/[^ ]*'))
+
+        if [[ ${#conflict_files[@]} > 0 ]]; then
+            echo -e "Conflict files detected. Resolving conflict files"
+            for ((i = 0; i < ${#conflict_files[@]}; i++)); do
+                sudo rm -rf ${conflict_files[$i]}
+                if [[ $? -eq 0 ]]; then
+                    echo -e "${GREEN}Removed: ${conflict_files[$i]} ${NC}"
+                else
+                    echo -e "${RED}Failed to remove: ${conflict_files[$i]} ${NC}"
+                fi
+            done
+        fi
+
+        conflict_packages=($(cat /tmp/installation.log | grep "are in conflict. Remove" | grep -o 'Remove [^ ]*' | grep -oE '[^ ]+$' | sed -e "s/[?]//"))
+
+        if [[ ${#conflict_packages[@]} > 0 ]]; then
+            echo -e "Conflict packages detected. Resovling conflict packages."
+            for ((i = 0; i < ${#conflict_packages[@]}; i++)); do
+                sudo pacman -Rcc --noconfirm ${conflict_packages[$i]} >/dev/null 2>&1
+                if [[ $? -eq 0 ]]; then
+                    echo -e "${GREEN}Uninstalled: ${conflict_packages[$i]} ${NC}"
+                else
+                    echo -e "${RED}Failed to uninstall: ${conflict_packages[$i]} ${NC}"
+                fi
+            done
+        fi
+
+        breakers=($(cat /tmp/installation.log | grep " breaks dependency " | grep -o 'required by [^ ]*' | grep -oE '[^ ]+$'))
+
+        if [[ ${#breakers[@]} > 0 ]]; then
+            echo -e "Conflict dependencies detected. Resovling conflicting dependencies."
+            for ((i = 0; i < ${#breakers[@]}; i++)); do
+                sudo pacman -Rdd --noconfirm ${breakers[$i]} >/dev/null 2>&1
+                if [[ $? -eq 0 ]]; then
+                    echo -e "${GREEN}Uninstalled: ${breakers[$i]} ${NC}"
+                else
+                    echo -e "${RED}Failed to uninstall: ${breakers[$i]} ${NC}"
+                fi
+            done
+        fi
+
+        satisfiers=($(cat /tmp/installation.log | grep "unable to satisfy dependency" | grep -oE '[^ ]+$'))
+
+        if [[ ${#satisfiers[@]} -gt 0 ]]; then
+            echo -e "Unsatisfied depencies detected. Resovling issues."
+            for ((i = 0; i < ${#satisfiers[@]}; i++)); do
+                sudo pacman -Rdd --noconfirm ${satisfiers[$i]} >/dev/null 2>&1
+                if [[ $? -eq 0 ]]; then
+                    echo -e "${GREEN}Uninstalled: ${satisfiers[$i]} ${NC}"
+                else
+                    echo -e "${RED}Failed to uninstall: ${satisfiers[$i]} ${NC}"
+                fi
+            done
+        fi
+
+        cp /tmp/installation.log "/tmp/installation${smart_install_retries}.log"
+        smart_install_retries=$((smart_install_retries + 1))
+
+        if [[ $smart_install_retries -gt 30 ]]; then
+            smart_install $@
+        else
+            continues=0
+        fi
     fi
 
 }
@@ -87,10 +193,10 @@ function safe_install() {
 function refresh_mirror() {
     sudo sed -i 's/Required[[:space:]]DatabaseOptional/Never/g' /etc/pacman.conf >/dev/null 2>&1
     sudo sed -i '/0x.sg/d' /etc/pacman.d/mirrorlist
-    sudo pacman -Sy --noconfirm archlinux-keyring >/dev/null 2>&1
+    smart_install archlinux-keyring
 
     sudo pacman -Qi reflector >/dev/null 2>&1
-    [[ $? -eq 1 ]] && sudo pacman -Sy reflector --noconfirm >/dev/null 2>&1
+    [[ $? -eq 1 ]] && smart_install reflector
     sudo reflector --country "Hong Kong" --country Singapore --country Japan --country China --latest 20 --protocol https --sort rate --save /etc/pacman.d/mirrorlist >/dev/null 2>&1
     sudo sed -i '/0x.sg/d' /etc/pacman.d/mirrorlist
 }
@@ -107,12 +213,11 @@ function remove_orphans() {
     [[ $? -eq 0 ]] && sudo pacman -Rcc --noconfirm fcitx-im >/dev/null 2>&1
     sudo pacman -Qi libreoffice-still >/dev/null 2>&1
     [[ $? -eq 0 ]] && sudo pacman -Rcc --noconfirm libreoffice-still >/dev/null 2>&1
-
-    sudo pacman -Rs $(sudo pacman -Qqtd) --noconfirm --quiet >/dev/null 2>&1
-
     # remove prvoise koompi theme
     sudo pacman -Qi breeze10-kde-git >/dev/null 2>&1
     [[ $? -eq 0 ]] && sudo pacman -Rcc --noconfirm breeze10-kde-git >/dev/null 2>&1
+
+    sudo pacman -Rs $(sudo pacman -Qqtd) --noconfirm --quiet >/dev/null 2>&1
 
     sudo rm -rf /usr/bin/theme-manager \
         /usr/share/applications/theme-manager.desktop \
@@ -194,22 +299,21 @@ function security_patch() {
     grep "include /usr/share/nano-syntax-highlighting/*.nanorc" /etc/nanorc >/dev/null 2>&1
     [[ $? -eq 1 ]] && echo -e "include /usr/share/nano-syntax-highlighting/*.nanorc" | sudo tee -a /etc/nanorc >/dev/null 2>&1
     # hostname
-    echo -e "koompi_os" | sudo tee /etc/hostname >/dev/null 2>&1
+    echo "koompi_os" | sudo tee /etc/hostname >/dev/null 2>&1
     # reflector
-    sudo pacman -Qi reflector >/dev/null 2>&1
-    [[ $? -eq 1 ]] && yes | sudo pamcan -S reflector >/dev/null 2>&1
     sudo systemctl enable reflector.service >/dev/null 2>&1
     echo -e '--save /etc/pacman.d/mirrorlist \n--country "Hong Kong" \n--country Singapore \n--country Japan \n--country China \n--latest 20 \n--protocol https --sort rate' | sudo tee /etc/xdg/reflector/reflector.conf >/dev/null 2>&1
 
     PRODUCT=$(cat /sys/class/dmi/id/product_name)
 
     if [[ ${PRODUCT} == "KOOMPI E11" ]]; then
-        safe_install rtl8723bu-git-dkms >/dev/null 2>&1
+        smart_install rtl8723bu-git-dkms >/dev/null 2>&1
     fi
 
-    sudo hwclock --systohc --localtime >/dev/null 2>&1
-    sudo timedatectl set-ntp true >/dev/null 2>&1
     sudo systemctl enable --now systemd-timedated systemd-timesyncd >/dev/null 2>&1
+    sudo timedatectl set-local-rtc 0 >/dev/null 2>&1
+    sudo timedatectl set-ntp 1 >/dev/null 2>&1
+    sudo hwclock --systohc --localtime >/dev/null 2>&1
 
     # Add to pix group for pix
     groups | grep "pix" >/dev/null 2>&1
@@ -229,7 +333,7 @@ function security_patch() {
 function install_upgrade() {
     sudo rm -rf /etc/skel
 
-    safe_install \
+    smart_install \
         koompi-wallpapers \
         koompi-plasma-themes \
         sddm-theme-koompi \
@@ -369,14 +473,14 @@ function install_upgrade() {
 
 function apply_new_theme() {
 
-    cp -r .bash_aliases ${HOME} >/dev/null 2>&1
-    cp -r .bash_history ${HOME} >/dev/null 2>&1
-    cp -r .bash_profile ${HOME} >/dev/null 2>&1
-    cp -r .bashrc ${HOME} >/dev/null 2>&1
-    cp -r .bash_script ${HOME} >/dev/null 2>&1
-    cp -r .config ${HOME} >/dev/null 2>&1
+    cp -r .bash_aliases ${HOME}
+    cp -r .bash_history ${HOME}
+    cp -r .bash_profile ${HOME}
+    cp -r .bashrc ${HOME}
+    cp -r .bash_script ${HOME}
+    cp -r .config ${HOME}
     mkdir -p /etc/sddm.conf.d/
-    echo -e '[Autologin]\nRelogin=false\nSession=\nUser=\n\n[General]\nHaltCommand=/usr/bin/systemctl poweroff\nRebootCommand=/usr/bin/systemctl reboot\n\n[Theme]\nCurrent=koompi-dark\n\n[Users]\nMaximumUid=60000\nMinimumUid=1000\n' | sudo tee /etc/sddm.conf.d/kde_settings.conf >/dev/null 2>&1
+    echo -e "[Autologin]\nRelogin=false\nSession=\nUser=\n\n[General]\nHaltCommand=/usr/bin/systemctl poweroff\nRebootCommand=/usr/bin/systemctl reboot\n\n[Theme]\nCurrent=koompi-dark\n\n[Users]\nMaximumUid=60000\nMinimumUid=1000\n" | sudo tee /etc/sddm.conf.d/kde_settings.conf >/dev/null 2>&1
     sh /usr/share/org.koompi.theme.manager/kmp-dark.sh >/dev/null 2>&1
 }
 
@@ -406,6 +510,12 @@ echo -e "${RED}NOTE: During update, do not turn off your computer.${NC}"
 echo -e ""
 
 if [[ continues -eq 1 ]]; then
+    (insert_koompi_repo) &
+    spinner "Updating the new repository of KOOMPI OS"
+    completed=$((completed + 1))
+fi
+
+if [[ continues -eq 1 ]]; then
     (remove_orphans) &
     spinner "Cleaning up unneed packages"
     completed=$((completed + 1))
@@ -418,14 +528,14 @@ if [[ continues -eq 1 ]]; then
 fi
 
 if [[ continues -eq 1 ]]; then
-    (insert_koompi_repo) &
-    spinner "Updating the new repository of KOOMPI OS"
+    (security_patch) &
+    spinner "Updating the default security configurations"
     completed=$((completed + 1))
 fi
 
 if [[ continues -eq 1 ]]; then
-    (security_patch) &
-    spinner "Updating the default security configurations"
+    (smart_update) &
+    spinner "Updating all installed applications"
     completed=$((completed + 1))
 fi
 
