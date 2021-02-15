70563cb643c5a0bdbefb555c1c370db29c3223f6
mrrbrilliant <mrrbrilliantwork@gmail.com>
2021-02-15 21:20:52 +0700

    removed test.sh

--- test.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-grep "dev.koompi.org" /etc/pacman.conf >/dev/null 2>&1
-[[ $? -eq 1 ]] && echo -e '\n[koompi]\nSigLevel = Never\nServer = https://dev.koompi.org/koompi\n' | sudo tee -a /etc/pacman.conf >/dev/null 2>&1
